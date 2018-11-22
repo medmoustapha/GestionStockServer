eff363c2eb292ceec1e430f77acba5e005dd1e2c
Mohamed Moustapha <medmostafaa@gmail.com>
2018-11-22 17:16:18 +0100

    commit 1

---
+++ src/main/java/gestion/stock/GestionStockServerApplication.java
@@ -0,0 +1,12 @@
+package gestion.stock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionStockServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestionStockServerApplication.class, args);
+	}
+}
